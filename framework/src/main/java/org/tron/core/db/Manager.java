a0a94ded3cc032da7439c4a6da7ca15859767e82
Heng Zhang <zhangheng1536@gmail.com>
2021-06-01 11:50:12 +0800

    remove "trxCap.setTrxTrace(null);"

--- framework/src/main/java/org/tron/core/db/Manager.java
+++ framework/src/main/java/org/tron/core/db/Manager.java
@@ -1238,7 +1238,7 @@ public class Manager {
     }
     //set the sort order
     trxCap.setOrder(transactionInfo.getFee());
-    trxCap.setTrxTrace(null);
+//    trxCap.setTrxTrace(null);
     return transactionInfo.getInstance();
   }
 
